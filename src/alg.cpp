b2d8d1c0f6bd2fb6ea0f74f74119deb0b2ba9939
PolinaGribkova <160274174+PolinaGribkova@users.noreply.github.com>
2024-03-10 20:19:23 +0300

    fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,40 +2,41 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
     for (uint64_t i = 2; i < value; i++) {
-        if (value % i == 0)
+        if (value % i == 0) {
             return false;
+        }
     }
-        return true;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
     uint64_t size = 0;
     uint64_t prost_arr[1000] = { 0 };
     for (int number = 2; size < 1000; number++) {
-        if (checkPrime(number)) 
-        prost_arr[size] = number;
-    size++;
+        if (checkPrime(number)) {
+            prost_arr[size] = number;
+            size++;
+        }
     }
-    return prost_arr[n];
+    return prost_arr[n-1];
 }
 
 uint64_t nextPrime(uint64_t value) {
-     value++;
-     while (checkPrime(value) == false) {
-         value++;
-     }
-     return value;
-};
+    value++;
+    while (checkPrime(value) == false) {
+        value++;
+    }
+    return value;
+}
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
-    for (int number = 2; number < hbound; number++) 
-    {
-        if (checkPrime(number))
+    for (int number = 2; number < hbound; number++) {
+        if (checkPrime(number)) {
             sum += number;
+        }
     }
     return sum;
 }
